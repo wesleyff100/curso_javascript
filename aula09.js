f27e11428bd405b4dead6aadae99c4564febcfc4
Wesley <wesleyff100@gmail.com>
2023-07-17 20:15:56 -0300

    Aula09

---
+++ aula09.js
@@ -0,0 +1,10 @@
+
+function soma(item1, item2) {
+    return item1 + item2
+}
+function diminuir(item1, item2) {
+    return item1 - item2
+}
+console.log('Soma:', soma(5, 6 ))
+console.log('Soma:', soma(12234,12312))
+console.log('Soma:', diminuir(12234,12312))
