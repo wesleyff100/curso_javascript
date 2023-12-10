67b704deed224d72d68817ebbcbd18dcdb72d8b0
Wesley <wesleyff100@gmail.com>
2023-12-10 17:50:41 -0300

    Aula 14

---
+++ aula14.js
@@ -0,0 +1,17 @@
+
+const mensagem ='Olá mundo!';
+const mensagem2 = "Olá mundo!";
+
+console.log(mensagem + mensagem2);
+
+console.log(mensagem.length);
+
+console.log(mensagem.substring(5));
+
+console.log(mensagem.substring(2, 8));
+
+console.log(mensagem.split(' '));
+
+console.log(mensagem.toLowerCase());
+
+console.log(mensagem.toUpperCase());
