d27e4f5570ae20bb2aac2615a78448c8a55f98b9
Wesley <wesleyff100@gmail.com>
2023-12-10 17:36:37 -0300

    Aula13

---
+++ aula13.js
@@ -0,0 +1,33 @@
+/* Escreva um algoritmo que leia 2 valores inteiros X e Y calcule a soma dos números que não são múltiplos de 13 entre X e Y, incluindo ambos.
+
+Entrada
+O arquivo de entrada contém 2 valores inteiros quaisquer, não necessariamente em ordem crescente.
+
+Saída
+Imprima a soma de todos os valores não divisíveis por 13 entre os dois valores lidos na entrada, inclusive ambos se for o caso.
+
+Exemplo de saída
+
+Entrada1: 100
+Entrada2: 200
+
+Saída: 13954
+*/
+
+function Multiplos13 (entrada1, entrada2){
+    let soma = 0;
+    for(let i = entrada1; i <= entrada2; i++){
+        if(i % 13 != 0){
+            soma += i;
+        }
+    }
+    return soma;
+}
+
+let entrada1 = 100;
+let entrada2 = 200;
+
+console.log(Multiplos13(entrada1, entrada2));
+
+
+
