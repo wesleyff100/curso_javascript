3661096acda97267b62bceaf0439e3581ffeefdb
Wesley <wesleyff100@gmail.com>
2023-12-10 21:28:13 -0300

    aula 19

---
+++ aula19.js
@@ -0,0 +1,19 @@
+
+function calcular() {
+    console.log('calcular');
+}
+
+const calcular2 = function() {
+    console.log('calcular 2');
+}   
+
+const calcular3 = () => {
+return 'abc';
+
+}
+const calcular4 = () => console.log('calcular 4');
+
+calcular();
+calcular2();
+calcular3();
+calcular4();
