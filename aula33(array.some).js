89990c0f828b0f721173f22e9abc8620f052c84d
Wesley <wesleyff100@gmail.com>
2023-12-18 18:32:21 -0300

    aula33

---
+++ aula33(array.some).js
@@ -0,0 +1,35 @@
+const lista = [
+    {
+        nome: 'Jose',
+        idade: 15,
+        exibir: true,
+    },
+    {
+        nome: 'Andre',
+        idade: 21,
+    },
+    {
+        nome: 'Pedro',
+        idade: 32,
+    },
+    {
+        nome: 'Jota',
+        idade: 45,
+    },
+    {
+        nome: 'Jora',
+        idade: 31,
+    },
+    {
+        nome: 'Jurasta',
+        idade: 29,
+    },
+];
+
+
+const pessoasSome = lista.some((objeto) => objeto.idade > 30);
+
+const pessoasEvery = lista.every((objeto) => objeto.idade > 30);
+
+console.log(pessoasSome);
+console.log(pessoasEvery);
